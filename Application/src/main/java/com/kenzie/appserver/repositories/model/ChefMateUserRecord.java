56ed1ef3367a2af244536ff309eed9d6dbffd892
Kelechi Obioha <kelechi.obioha@mykenzie.snhu.edu>
2023-03-05 02:48:50 -0600

    ChefMateUser repository and record

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/ChefMateUserRecord.java
@@ -0,0 +1,63 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.List;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "ChefMateUser")
+public class ChefMateUserRecord {
+    private String userId;
+    private String userName;
+    private List<String> userPreferences;
+    private Set<String> recipesTried;
+    private Set<String> ingredients;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @DynamoDBAttribute(attributeName = "userName")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @DynamoDBAttribute(attributeName = "userPreferences")
+    public List<String> getUserPreferences() {
+        return userPreferences;
+    }
+
+    public void setUserPreferences(List<String> userPreferences) {
+        this.userPreferences = userPreferences;
+    }
+
+    @DynamoDBAttribute(attributeName = "recipesTried")
+    public Set<String> getRecipesTried() {
+        return recipesTried;
+    }
+
+    public void setRecipesTried(Set<String> recipesTried) {
+        this.recipesTried = recipesTried;
+    }
+
+    @DynamoDBAttribute(attributeName = "ingredients")
+    public Set<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+}
